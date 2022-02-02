a6f109a3fad52a4dc9ecf3de3ad4eb4f158dfce1
Oscar Silva <oscarsilva2356@gmail.com>
2022-02-01 21:26:36 -0800

    update to the schema.sql and seed.sql

---
+++ db/seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO department (id, department_name)
+VALUES (1, "Engineering"),
+       (2, "Legal"),
+       (3. "Sales"), 
+       (4, "Finance") 
+
+INSERT INTO role (id, title, salary, department)
+VALUES (1, "Sales person", 100000, 3)
+       (2, "Lawyer", 200000, 2)
+       
+       
