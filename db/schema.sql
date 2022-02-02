4217446781e5656e8ec474a5c91605fd756df15d
Oscar Silva <oscarsilva2356@gmail.com>
2022-02-01 23:41:24 -0800

    working in schema

--- db/schema.sql
+++ db/schema.sql
@@ -9,22 +9,22 @@ CREATE TABLE department (
 ); 
 
 CREATE TABLE role (
-    id NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    id  INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     title VARCHAR(30) NOT NULL, 
     salary DECIMAL NOT NULL, 
-    department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    department_id INT NOT NULL, 
     FOREIGN KEY (department_id) 
     REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     first_name VARCHAR(30) NOT NULL, 
     last_name VARCHAR(30) NOT NULL, 
     role_id INT NOT NULL, 
-    FOREIGN KEY(role_id)
-    REFERENCES role(id)
-    manager_id INT NOT NULL, 
-    FOREIGN KEY (employee_id)
-    REFERENCES employee(id)
-);
+    FOREIGN KEY(role_id) REFERENCES role(id)
+    manager_id INT, 
+    FOREIGN KEY(manager_id) REFERENCES employee(id)
+); 
+
+
--- db/seed.sql
+++ db/seed.sql
@@ -2,10 +2,16 @@ INSERT INTO department (id, department_name)
 VALUES (1, "Engineering"),
        (2, "Legal"),
        (3. "Sales"), 
-       (4, "Finance") 
+       (4, "Finance");
 
 INSERT INTO role (id, title, salary, department)
-VALUES (1, "Sales person", 100000, 3)
+VALUES (1, "Engineer", 100000, 1)
        (2, "Lawyer", 200000, 2)
-       
-       
+       (3, "Sales Person", 90000, 3)
+       (4, "Financer", 100000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)   
+VALUES ("Chris", "Lee", 2, 5)
+       ("Ann", "Carter", 1, 6)
+       ("David", "Smith", 3, 7)
+       ("Max", "Keegan", 4, 8);
