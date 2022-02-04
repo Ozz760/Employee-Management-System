077a05809c504e1083b61317a86943f6c52428cf
Oscar Silva <oscarsilva2356@gmail.com>
2022-02-04 00:06:41 -0800

    added ReadME

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Employee-Management-System
+
+---
+## Description 
+
+This is an app where you are able to view, add, or update any information in the company. It is an interactable interface were manager and the like can manage their employees. 
+
+---
+## Installation 
+You will need to run and install mysql2 package, inquirer package, and the console.table package. 
+
+---
+## Built With: 
+Node.js 
+
+MySQL
+
+JavaScript 
+
+Inquirer
+
+---
+## Authors 
+Oscar Silva 
+
+--- 
+## Questions 
+Github: https://github.com/Ozz760
+
+Email: oscarsilva2356@gmail.com
--- server.js
+++ server.js
@@ -9,216 +9,199 @@ const db = mysql.createConnection({
   database: "employee_db",
 });
 
-const beginPrompt = () => {
-  inquirer
-    .prompt({
-      type: "list",
-      name: "opener",
-      message: "What would you like to do?",
-      choices: [
-        "View all employees",
-        "Add employees",
-        "Update employee role",
-        "View all roles",
-        "Add role",
-        "View all departments",
-        "Add department",
-        "Quit",
-      ],
-    })
-    .then((answer) => {
-      switch (answer.opener) {
-        case "View all employees":
-          viewAllEmployees();
-          break;
-        case "Add employees":
-          addEmployee();
-          break;
-        case "Update employees role":
-          updateEmployeesRole();
-          break;
-        case "View all roles":
-          viewRoles();
-          break;
-        case "Add role":
-          addRole();
-          break;
-        case "View all departments":
-          viewAllDepartments();
-          break;
-        case "Add department":
-          addDepartment();
-          break;
-        case "Quit":
-          quit();
-          break;
-      }
-    });
-};
-
-const viewAllEmployees = () => {
-  db.query(`SELECT first_name, last_name FROM employee`, (err, res) => {
-    if (err) {
-      throw err;
-    } else {
-      console.table(res);
-    }
-    beginPrompt();
-  });
-};
-
-const viewRoles = () => {
-  db.query(`SELECT title FROM role`, (err, res) => {
-    if (err) {
-      throw err;
-    } else {
-      console.table(res);
-    }
-    beginPrompt();
-  });
-};
-
-const viewAllDepartments = () => {
-  db.query(`SELECT * FROM department`, (err, res) => {
-    if (err) {
-      throw err;
-    } else {
-      console.table(res);
-    }
-    beginPrompt();
-  });
-};
-
-const addEmployee = () => {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "firstName",
-        message: "Enter first name",
-      },
-      {
-        type: "input",
-        name: "lastName",
-        message: "Enter last name",
-      },
-      {
-        type: "input",
-        name: "newRoleId",
-        message: "Enter job ID",
-      },
-      {
-        type: "input",
-        name: "newManId",
-        message: "Enter manager ID",
-      },
-    ])
-    .then((answers) => {
-      db.query(
-        `INSERT INTO employee(first_name, last_name, job_id, manager_id) values ('${answers.firstName}', '${answers.lastName}', '${answers.newRoleId}', '${answers.newManId}')`
-      ),
-        (err, res) => {
-          if (err) {
-            throw err;
-          } else {
-            console.table(res);
-          }
-          beginPrompt();
-        };
-    });
-};
-
-const addRole = () => {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "newRole",
-        message: "Enter title",
-      },
-      {
-        type: "input",
-        name: "newSalary",
-        message: "Enter salary",
-      },
-      {
-        type: "input",
-        name: "newDepId",
-        message: "Enter Department ID",
-      },
-    ])
-    .then((answers) => {
-      db.query(
-        `INSERT INTO roles(title, salary, department_id) values ('${answers.newRole}', '${answers.newSalary}', '${answers.newDepId}')`
-      ),
-        (err, res) => {
-          if (err) {
-            throw err;
-          } else {
-            console.table(res);
-          }
-          beginPrompt();
-        };
-    });
-};
-
-const addDepartment = () => {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "newDepartment",
-        message: "Enter department",
-      },
-    ])
-    .then((answers) => {
-      db.query(
-        `INSERT INTO department(name) values ('${answers.newDepartment}')`
-      ),
-        (err, res) => {
-          if (err) {
-            throw err;
-          } else {
-            console.table(res);
-          }
-          beginPrompt();
-        };
-    });
-};
-
-const updateEmployeesRole = () => {
-  db.query("SELECT * from employee", (err, res) => {
-    if (err) {
-      throw err;
-    } else {
-      console.table(res);
-    }
+function beginPrompt() {
     inquirer
-      .prompt([
+      .prompt([{
+        type: "list",
+        name: "beginPrompt",
+        message: "Select an option",
+        choices: ["View All Employees",
+          "Add Employee",
+          'Update Employee Job',
+          'View All Jobs',
+          'Add Job',
+          "View All Departments",
+          'Add Departments'
+        ],
+      }, ])
+      .then((answers) => {
+        switch (answers.beginPrompt) {
+          case 'View All Employees':
+            viewEmployees();
+            break
+          case 'Add Employee':
+            addEmployee();
+            break
+          case 'Update Employee Job':
+            updateEmployee();
+            break
+          case 'View All Jobs':
+            viewJobs();
+            break
+          case 'View All Departments':
+            viewDepartments();
+            break
+          case 'Add Job':
+            addJob();
+            break
+          case 'Add Departments':
+            addDepartment();
+        }
+      });
+  }
+  
+
+  const viewDepartments = () => {
+    db.query('SELECT * from department', (err, res) => {
+      if (err) {
+        throw err
+      } else {
+        console.table(res)
+      }
+      beginPrompt();
+    })
+  }
+  
+ 
+  const viewEmployees = () => {
+    db.query('SELECT first_name, last_name from employee', (err, res) => {
+      if (err) {
+        throw err
+      } else {
+        console.table(res)
+      }
+      beginPrompt();
+    })
+  }
+  
+
+  const viewJobs = () => {
+    db.query('SELECT title from job', (err, res) => {
+      if (err) {
+        throw err
+      } else {
+        console.table(res)
+      }
+      beginPrompt();
+    })
+  }
+  
+
+  const addEmployee = () => {
+    inquirer
+      .prompt([{
+          type: "input",
+          name: "firstName",
+          message: "Enter first name",
+        },
         {
           type: "input",
-          name: "employeeTarget",
-          message: "Enter employee ID to make changes",
+          name: "lastName",
+          message: "Enter last name",
         },
         {
           type: "input",
           name: "newJobId",
-          message: "Enter new job ID",
+          message: "Enter job ID",
         },
+        {
+          type: "input",
+          name: "newManId",
+          message: "Enter manager ID",
+        }
       ])
       .then((answers) => {
-        db.query(
-          `UPDATE employee SET job_id = ${answers.newJobId} WHERE id = ${answers.employeeTarget}`
-        ),
-          (err, res) => {
-            if (err) {
-              throw err;
-            } else {
-            }
-            beginPrompt();
-          };
+        db.query(`INSERT INTO employee(first_name, last_name, job_id, manager_id) values ('${answers.firstName}', '${answers.lastName}', '${answers.newJobId}', '${answers.newManId}')`, (err, res) => {
+          if (err) {
+            throw err
+          } else {
+            console.table(res)
+          }
+          beginPrompt();
+        });
       });
-  });
-};
+  }
+  
 
-beginPrompt();
+  const addDepartment = () => {
+    inquirer
+      .prompt([{
+        type: "input",
+        name: "newDepartment",
+        message: "Enter department ID",
+      }])
+      .then((answers) => {
+        db.query(`INSERT INTO department(name) values ('${answers.newDepartment}')`, (err, res) => {
+          if (err) {
+            throw err
+          } else {
+            console.table(res)
+          }
+          beginPrompt();
+        });
+      });
+  }
+  
+  
+  const addJob = () => {
+    inquirer
+      .prompt([{
+          type: "input",
+          name: "newRole",
+          message: "Enter title",
+        },
+        {
+          type: "input",
+          name: "newSalary",
+          message: "Enter salary",
+        },
+        {
+          type: "input",
+          name: "newDepId",
+          message: "Enter Department ID",
+        }
+      ])
+      .then((answers) => {
+        db.query(`INSERT INTO job(title, salary, department_id) values ('${answers.newRole}', '${answers.newSalary}', '${answers.newDepId}')`, (err, res) => {
+          if (err) {
+            throw err
+          } else {
+            console.table(res)
+          }
+          beginPrompt();
+        });
+      });
+  }
+  
+
+  const updateEmployee = () => {
+    db.query('SELECT * from employee', (err, res) => {
+      if (err) {
+        throw err
+      } else {
+        console.table(res)
+      }
+      inquirer
+        .prompt([{
+            type: "input",
+            name: "employeeTarget",
+            message: "Enter employee ID to make changes",
+          },
+          {
+            type: "input",
+            name: "newJobId",
+            message: "Enter new job ID",
+          },
+        ])
+        .then((answers) => {
+          db.query(`UPDATE employee SET job_id = ${answers.newJobId} WHERE id = ${answers.employeeTarget}`);
+          if (err) {
+            throw err
+          } else {
+          }
+          beginPrompt();
+        });
+    })
+  }
+  
+  beginPrompt();
